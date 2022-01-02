5665cff9e7c6f39593c3204fdec8105774a3c9b6
Eugenio Tampieri <eugenio@eutampieri.eu>
2022-01-02 10:42:30 +0100

    Link to product

--- templates/home.php
+++ templates/home.php
@@ -1,13 +1,16 @@
 <?php
 require_once(dirname(dirname(__FILE__))."/utils.php");
 ?>
-<div id="showlist" class="family d-flex flex-wrap justify-content-around">
+<div class="family d-flex flex-wrap justify-content-around">
   <?php foreach($products as $product): ?>
-  <div class="card m-2" style="width: 15rem;">
-    <img src="<?= $product["picture"] ?>" class="card-img-top"
-      alt="">
+  <div class="card m-2 product" style="width: 15rem;">
+    <a href="/product.php?id=<?= $product["id"] ?>">
+    <img src="<?= $product["image"] ?>" class="card-img-top"
+      alt=""></a>
     <div class="card-body">
+      <a href="/product.php?id=<?= $product["id"] ?>">
       <h2><?= $product["title"] ?></h2>
+      </a>
       <a href="." class="btn btn-primary showlink"><i class="fa fa-cart-plus" aria-hidden="true"></i> <?= price_to_string($product["price"])?></a>
     </div>
   </div>
