300e4e50a0f9b04417e1630b91ae4a3b2b62c67d
Michael Chelli <michaelchelli00@gmail.com>
2022-01-02 11:36:37 +0100

    small fixes

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 $page_title = "Home";
 $head_template = "page_head.php";
 $body_template = "page.php";
-$page_content_template = "cart.php";
+$page_content_template = "home.php";
 $products = [
 	[
 				"image" => "",
