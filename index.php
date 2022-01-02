aaa2653bbb95530900572266441c38ab61de1f02
Michael Chelli <michaelchelli00@gmail.com>
2022-01-02 11:32:59 +0100

    cart page

--- index.php
+++ index.php
@@ -2,11 +2,13 @@
 $page_title = "Home";
 $head_template = "page_head.php";
 $body_template = "page.php";
-$page_content_template = "home.php";
+$page_content_template = "cart.php";
 $products = [
-    [
+	[
+				"image" => "",
         "title" => "Lorem Ipsum",
-        "price" => 1299,
+				"price" => 1299,
+				"quantity" => 3,
     ]
 ];
 require_once("templates/main.php");
---
+++ templates/cart.php
@@ -0,0 +1,28 @@
+<?php
+require_once(dirname(dirname(__FILE__))."/utils.php");
+?>
+<h1>Checkout</h1>
+<form id="cart">
+	<?php foreach($products as $product): ?>
+	<div class="row">
+		<div class = "col-4">
+			<img src="<?= $product['image']?>" alt="">
+		</div>
+		<div class = "col-8">
+			<h2><?= $product['title'] ?></h2>
+			<p><label>quantity <input id="product-<?= $product['id']?>-quantity" type="number" value="<?= $product['quantity'] ?>" /></label> x <?= price_to_string($product['price']) ?></p>
+		</div>
+	</div>
+	<?php endforeach; ?>
+</form>
+<div class="row">
+	<div class="col">Spese di spedizione</div>
+	<div class="col"><?= price_to_string(500) ?></div>
+</div>
+<div class="row">
+	<div class="col">Totale ordine</div>
+	<div class="col"><?= price_to_string(array_reduce(array_map(fn($product):int => $product['price']*$product['quantity'],$products), fn($a, $b):int => $a+$b)) ?></div>
+</div>
+<div class="row"><button type="button" class="btn btn-primary">Apple pay (fast checkout)</button></div>
+<div class="row"><button type="button" class="btn btn-primary">Google pay (fast checkout)</button></div>
+<div class="row"><button type="button" class="btn btn-primary">Paga con carta</button></div>
