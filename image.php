c2c69999fa263e6fde2ad2f768fcbd6db3dcf13e
Eugenio Tampieri <eugenio@eutampieri.eu>
2022-01-02 13:18:13 +0100

    Fetch images from DB

--- image.php
+++ image.php
@@ -10,4 +10,11 @@ if(count($result) != 1) {
     header("Location: https://picsum.photos/1000/1000");
 } else if($result[0]["image"] === null) {
     header("Location: https://picsum.photos/1000/1000");
+} else {
+    $finfo = new finfo(FILEINFO_MIME_TYPE);
+    $mime = $finfo->buffer($result[0]["image"]);
+    header("Content-Type: $mime");
+    header("Cache-Control: public, max-age=31536000");
+    header("Content-Length: ".strlen($result[0]["image"]));
+    echo $result[0]["image"];
 }
