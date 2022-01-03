bbc3d46d3489cc6df40ea24d496d9a0589dc3396
Eugenio Tampieri <eugenio@eutampieri.eu>
2022-01-03 19:07:53 +0100

    User page

--- me.php
+++ me.php
@@ -17,6 +17,8 @@ if(count($users) != 1) {
     die();
     // TODO Clean session since it's invalid and redirect
 }
+$user = $users[0];
+
 $stmt = $db->prepare("SELECT `order`.id, MIN(order_update.timestamp) AS `date` FROM `order`, order_update WHERE `order`.user_id = :uid AND `order`.id = order_update.order_id GROUP BY order_update.order_id ORDER BY `date` DESC");
 $stmt->bindParam(":uid", $_SESSION["user_id"]);
 $stmt->execute();
