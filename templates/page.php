2212b46e4cd87a54e11fdc3ab80dfd0968c893c6
Eugenio Tampieri <eugenio@eutampieri.eu>
2022-01-03 17:13:52 +0100

    Changed nav to header

--- templates/page.php
+++ templates/page.php
@@ -1,6 +1,6 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light mr-auto">
+<header class="navbar navbar-expand-lg navbar-light bg-light mr-auto">
     <span class="navbar-brand"><?= $page_title ?></span>
-</nav>
+</header>
 <div class="container">
     <?php
     require_once($page_content_template);
