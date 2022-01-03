3d38cdcd6a3b230299b3f4532e8540327e4df224
Eugenio Tampieri <eugenio@eutampieri.eu>
2022-01-03 15:02:22 +0100

    Go to pay.php

--- templates/cart_t.php
+++ templates/cart_t.php
@@ -2,7 +2,8 @@
 require_once(dirname(dirname(__FILE__))."/utils.php");
 ?>
 <h1>Checkout</h1>
-<form id="cart">
+<form id="cart" method="POST" action="pay.php">
+	<input type="hidden" name="create_checkout">
 	<?php foreach($cart as $cart_item): ?>
 	<div class="row">
 		<div class = "col-4">
@@ -14,15 +15,16 @@ require_once(dirname(dirname(__FILE__))."/utils.php");
 		</div>
 	</div>
 	<?php endforeach; ?>
+	<div class="row">
+		<div class="col">Spese di spedizione</div>
+		<div class="col"><?= price_to_string($delivery_price) ?></div>
+	</div>
+	<div class="row">
+		<div class="col">Totale ordine</div>
+		<div class="col"><?= price_to_string(array_reduce(array_map(fn($product):int => $product['price']*$product['quantity'],$cart), fn($a, $b):int => $a+$b)) ?></div>
+	</div>
+	<div class="row">
+		<div class="col-sm-12 col-md-3 m-auto"><button type="button" class="w-100 mt-3 m-auto btn btn-primary m-2">Paga ora</button></div>
+	</div>
 </form>
-<div class="row">
-	<div class="col">Spese di spedizione</div>
-	<div class="col"><?= price_to_string($delivery_price) ?></div>
-</div>
-<div class="row">
-	<div class="col">Totale ordine</div>
-	<div class="col"><?= price_to_string(array_reduce(array_map(fn($product):int => $product['price']*$product['quantity'],$cart), fn($a, $b):int => $a+$b)) ?></div>
-</div>
-<div class="row"><button type="button" class="btn btn-primary m-2">Apple pay (fast checkout)</button></div>
-<div class="row"><button type="button" class="btn btn-primary m-2">Google pay (fast checkout)</button></div>
-<div class="row"><button type="button" class="btn btn-primary m-2">Paga con carta</button></div>
+	
