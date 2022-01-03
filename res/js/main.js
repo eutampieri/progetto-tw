4906376548947f746d2be937b20e523b8a02c920
Eugenio Tampieri <eugenio@eutampieri.eu>
2022-01-03 17:10:33 +0100

    Show modal to go to cart

--- res/js/main.js
+++ res/js/main.js
@@ -26,9 +26,38 @@ async function update_item_quantity(product, quantity) {
 	return await fetch("/edit-cart.php?product_id=" + product + "&quantity=" + quantity).then(x => x.json());
 }
 
+function create_go_to_cart_modal() {
+	for (const modal of document.getElementsByClassName("modal")) {
+		modal.remove();
+	}
+	const modalContent = `
+  <div class="modal-dialog">
+	<div class="modal-content">
+	  <div class="modal-header">
+		<h5 class="modal-title">Vui andare al carrello?</h5>
+	  </div>
+	  <div class="modal-body">
+	  </div>
+	  <div class="modal-footer">
+		<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Chiudi</button>
+		<a class="btn btn-primary" role="button" href="/cart.php">Vai al carrello</a>
+	  </div>
+	</div>
+  </div>`;
+	let root = document.createElement("div");
+	root.className = " modal fade";
+	root.tabIndex = -1;
+	root.ariaLabel = "Vai al carrello";
+	root.ariaHidden = true;
+	root.innerHTML = modalContent;
+	document.getElementsByTagName("body")[0].appendChild(root);
+	return root;
+}
+
 
 async function update_item_quantity_btn(button) {
 	let cart = await update_item_quantity(button.dataset["product"], button.dataset["increment"]);
+	new bootstrap.Modal(create_go_to_cart_modal()).show();
 	/*for (const item of cart.items) {
 		if(item.id == button.dataset["product"]) {
 			document.getElementById("")
