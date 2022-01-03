d5c9bf6accd6c23bbd2f002bb46859ff33005c80
Michael Chelli <michaelchelli00@gmail.com>
2022-01-03 14:08:54 +0100

    fixed display issues

--- templates/cart_t.php
+++ templates/cart_t.php
@@ -4,27 +4,31 @@ require_once(dirname(dirname(__FILE__))."/utils.php");
 <script>
 async function update_cart(item) {
 	let diff=parseInt(item.value)-parseInt(item.dataset["oldvalue"]);
-	item.dataset["oldvalue"]=item.value;
+	item.disabled=true;
 	let cart = await update_item_quantity(parseInt(item.id), diff);
 	let delivery_price = cart.delivery_price;
 	let total_price=delivery_price;
-	for(const item of cart.items) {
-		total_price+=parseInt(item.price)*parseInt(item.quantity);
+	for(const cart_item of cart.items) {
+		total_price+=parseInt(cart_item.price)*parseInt(cart_item.quantity);
+		if(item.id == cart_item.product_id) {
+			item.dataset["oldvalue"]=cart_item.quantity;
+		}
 	}
 	document.getElementById("total_price").innerHTML=((total_price/100.0).toFixed(2)+" &euro;").replace('.',',');
 	document.getElementById("delivery_price").innerHTML=((delivery_price/100.0).toFixed(2)+" &euro;").replace('.',',');
+	item.disabled=false;
 }
 </script>
 <h1>Checkout</h1>
 <form id="cart">
 	<?php foreach($cart as $cart_item): ?>
 	<div class="row">
-		<div class="col-1">
-			<img src="/image.php?id=<?= $product["id"] ?>" alt="" class="w-100"></a>
+		<div class="col-3 col-lg-2">
+			<img src="/image.php?id=<?= $cart_item["product_id"] ?>" alt="" class="w-100"></a>
 		</div>
-		<div class = "col-11">
+		<div class="col-9 col-lg-10">
 			<h2><?= $cart_item['title'] ?></h2>
-			<p><label>quantity <input id="<?= $cart_item['id']?>" type="number" min=0 value="<?= $cart_item['quantity'] ?>" data-oldvalue="<?= $cart_item['quantity'] ?>" onchange="update_cart(this);" /></label> x <?= price_to_string($cart_item['price']) ?></p>
+			<p><label>quantity <input id="<?= $cart_item['product_id']?>" type="number" style="width:3em;" min=0 step=1 value="<?= $cart_item['quantity'] ?>" data-oldvalue="<?= $cart_item['quantity'] ?>" onchange="update_cart(this);" /></label> x <?= price_to_string($cart_item['price']) ?></p>
 		</div>
 	</div>
 	<?php endforeach; ?>
