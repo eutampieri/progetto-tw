4c1afd91722453e56dd997278d847a247597eef1
Eugenio Tampieri <eugenio@eutampieri.eu>
2022-01-03 10:58:32 +0100

    Created JS folder

--- res/js/main.js
+++ res/js/main.js
@@ -2,18 +2,18 @@
 
 async function display_order(order_id) {
 	let res = createElement("div");
-	fetch("/status_api.php?order_id="+order_id).then(response => response.json()).then(data => {
-		for(let x in data) {
-			if Array.isArray(data[x]) {
-				for(let y in data[x]) {
+	fetch("/status_api.php?order_id=" + order_id).then(response => response.json()).then(data => {
+		for (let x in data) {
+			if (Array.isArray(data[x])) {
+				for (let y in data[x]) {
 					let p = createElement("p");
-					let t = createTextNode(x+": "+data[x][y]);
+					let t = createTextNode(x + ": " + data[x][y]);
 					p.appendChild(t);
 					res.appendChild(p);
 				}
 			} else {
 				let p = createElement("p");
-				let t = createTextNode(x+": "+data[x]);
+				let t = createTextNode(x + ": " + data[x]);
 				p.appendChild(t);
 				res.appendChild(p);
 			}
