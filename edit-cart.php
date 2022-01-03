f0076c9f7baca0e9d8f47c61b2a78f7fe158025a
Eugenio Tampieri <eugenio@eutampieri.eu>
2022-01-03 10:51:42 +0100

    Since we no longer autoincrement, we hae to unassign empty carts to users

--- edit-cart.php
+++ edit-cart.php
@@ -36,5 +36,17 @@ $query->execute();
 $stmt = $db->prepare("DELETE FROM cart WHERE quantity <= 0");
 $stmt->execute();
 
+$stmt = $db->prepare("SELECT COUNT(*) as n FROM cart WHERE id = :id");
+$stmt->bindParam(":id", $_SESSION["cart_id"]);
+$stmt->execute();
+if($stmt->fetchAll(PDO::FETCH_ASSOC)[0]["n"] == 0) {
+    unset($_SESSION["cart_id"]);
+    if(isset($_SESSION["user_id"])){
+        $query = $db->prepare("UPDATE user SET cart_id = NULL WHERE id = :uid;");
+        $query->bindParam(":uid", $_SESSION["user_id"]);
+        $query->execute();
+    }
+}
+
 http_response_code(303);
 header("Location: /cart.php?json");
