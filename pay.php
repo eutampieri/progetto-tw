2d684890dd3d0f5f8fb8c4cf52131e63e4978808
Eugenio Tampieri <eugenio@eutampieri.eu>
2022-01-03 19:55:03 +0100

    Use user email instead of placeholder

--- pay.php
+++ pay.php
@@ -15,9 +15,15 @@ if(isset($_REQUEST["create_checkout"]) && isset($_SESSION["cart_id"])){
     $cart_query->bindParam(":cart_id",$_SESSION["cart_id"]);
     $cart_query->execute();
     $cart = $cart_query->fetchAll(PDO::FETCH_ASSOC);
+
+    $stmt = $db->prepare("SELECT email FROM user WHERE id = :id");
+    $stmt->bindParam(":id", $_SESSION["user_id"]);
+    $stmt->execute();
+    $email = $db->fetchAll(PDO::FETCH_ASSOC)[0]["email"];
+
     $delivery_price = calc_delivery_price($cart);
     array_push($cart, ["name" => "Shipping cost", "quantity" => 1, "price" => $delivery_price]);
-    $stripe_session = $stripe->create_session($cart, "https://example.com", "https://example.com", "ginopino@blog.tw.com");
+    $stripe_session = $stripe->create_session($cart, "https://example.com", "https://example.com", $email);
     $_SESSION["payment_intent"] = $stripe_session["payment_intent"];
     http_response_code(303);
     header("Location: ".$stripe_session["url"]);
