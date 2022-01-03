55942cc6532d8926ac1b2dc940a30759c01cc2b7
Eugenio Tampieri <eugenio@eutampieri.eu>
2022-01-03 17:42:02 +0100

    Fixed dumb mistake

--- templates/page.php
+++ templates/page.php
@@ -1,6 +1,6 @@
 <header class="navbar navbar-expand-lg navbar-light bg-light mr-auto d-flex p-3">
     <span class="navbar-brand"><?= $page_title ?></span>
-    <?php if(!isset($cart_count)): ?>
+    <?php if(isset($cart_count)): ?>
     <a class="btn btn-primary ms-auto me-1" href="/cart.php" role="button">
         <i class="fa fa-shopping-cart" aria-label="Carrello: "></i>
         <span id="cart-count" class="badge rounded-pill bg-light text-dark"><?= $cart_count ?></span>
