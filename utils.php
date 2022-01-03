218d7c2dec0f0d2e79f661fc1d98043afb09e6ed
Eugenio Tampieri <eugenio@eutampieri.eu>
2022-01-03 15:47:44 +0100

    Fix stripe client

--- utils.php
+++ utils.php
@@ -33,23 +33,21 @@ class Stripe
     function create_session($bill, $callback_ok, $callback_cancel, $email) {
         $bill_rows = [];
         foreach($bill as $i => $x) {
-            error_log(json_encode([$i, $x]));
             array_push($bill_rows, "line_items[$i][price_data][currency]=eur&&line_items[$i][price_data][product_data][name]=".$x["name"]."&line_items[$i][price_data][unit_amount]=".$x["price"]."&line_items[$i][quantity]=".$x["quantity"]);
         }
         $query = "payment_method_types[]=card&mode=payment&success_url=".urlencode($callback_ok)."&cancel_url=".urlencode($callback_cancel)."&payment_intent_data[capture_method]=manual&customer_email=".urlencode($email)."&".implode("&", $bill_rows);
-        error_log($query);
-        $resp = generic_request("https://api.stripe.com/v1/checkout/sessions", $query, ["Authorization: Basic ".base64_encode($this->secret.':')], "POST");
+        $resp = json_decode(http_request("https://api.stripe.com/v1/checkout/sessions", $query, ["Authorization: Basic ".base64_encode($this->secret.':')], "POST"), true);
         return [
             "url"=>$resp["url"],
             "payment_intent" => $resp["payment_intent"]
         ];
     }
     function get_payment_status($intent) {
-        $resp = generic_request("https://api.stripe.com/v1/payment_intents/$intent", "", ["Authorization: Basic ".base64_encode($this->secret.':')]);
+        $resp = json_decode(http_request("https://api.stripe.com/v1/payment_intents/$intent", "", ["Authorization: Basic ".base64_encode($this->secret.':')]), true);
         return $resp["amount"] == $resp["amount_capturable"] && $resp["amount_received"] == 0;
     }
     function capture_payment($intent) {
-        generic_request("https://api.stripe.com/v1/payment_intents/$intent/capture", "", ["Authorization: Basic ".base64_encode($this->secret.':')], "POST");
+        http_request("https://api.stripe.com/v1/payment_intents/$intent/capture", "", ["Authorization: Basic ".base64_encode($this->secret.':')], "POST");
     }
 }
 
