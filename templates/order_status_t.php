a99a7421aef982d695c50de23128a06850381259
Michael Chelli <michaelchelli00@gmail.com>
2022-01-03 15:35:32 +0100

    gotta change branch

--- templates/order_status_t.php
+++ templates/order_status_t.php
@@ -1,4 +1,11 @@
+<?= require("cart_t.php") ?>
 <main class="row" id="main_content">
+<div class="family d-flex flex-wrap justify-content-around">
+  <?php foreach($order as $order_elements): ?>
+    <img src="/image.php?id=<?= $order_element["product_id"] ?>" class="card-img-top" alt="">
+		<p>Quantity: <?= $order_element["quantity"] ?></p>
+  <?php endforeach; ?>
+</div>
 </main>
 <script>
 	async function load(){
