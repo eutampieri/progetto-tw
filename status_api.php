cccc1e4144bcfb9801d9214c81fd359bf7d23832
Eugenio Tampieri <eugenio@eutampieri.eu>
2022-01-04 18:38:03 +0100

    Return payment data

--- status_api.php
+++ status_api.php
@@ -1,11 +1,12 @@
 <?php
 	require_once("utils.php");
+	$stripe = new Stripe("sk_test_wWygRumClv9lRAWpxQyLyzgD00oDfv5zAD");
 	$db = get_db();
 	$order_id = $_GET['order_id'];
 	if(!is_numeric($order_id)) {
 		die("order_id must be a number");
 	}
-	$order_query = $db->prepare("select * from `order` left join express_courier on express_courier_id = `order`.express_courier_id where `order`.id=:order_id");
+	$order_query = $db->prepare("select cart_id, payment_id, tracking_number, name as courier_name from `order` left join express_courier on express_courier_id = `order`.express_courier_id where `order`.id=:order_id");
 	$order_query->bindParam(":order_id", $order_id);
 	$order_query->execute();
 	$res['order'] = $order_query->fetch(PDO::FETCH_ASSOC);
@@ -17,6 +18,15 @@
 	$cart_query->bindParam(":cart_id", $res['order']["cart_id"]);
 	$cart_query->execute();
 	$res['cart'] = $cart_query->fetchAll(PDO::FETCH_ASSOC);
+
+	$stripe_data = $stripe->get_payment_details($res['order']["payment_id"]);
+	$res["payment_infos"] = $stripe_data["charges"]["data"][0]["payment_method_details"]["card"];
+	unset($res['order']["payment_id"]);
+	unset($res['order']["cart_id"]);
+	unset($res['payment_infos']["fingerprint"]);
+	unset($res['payment_infos']["exp_month"]);
+	unset($res['payment_infos']["exp_year"]);
+	$res["payment_infos"]["receipt"] = $stripe_data["charges"]["data"][0]["receipt_url"];
 
 	header("Content-Type: application/json");
 	echo json_encode($res);
