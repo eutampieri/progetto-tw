6202655676bf03c59f5ee2eb02e4ba09342131b6
Eugenio Tampieri <eugenio.tampieri@studio.unibo.it>
2022-01-10 10:40:11 +0100

    Fix heading levels

--- templates/cart_t.php
+++ templates/cart_t.php
@@ -25,7 +25,7 @@ function add(item_id, delta) {
 	update_cart(item);
 }
 </script>
-<h1>Carrello</h1>
+<h2>Carrello</h2>
 <form id="cart" method="POST" action="pay.php">
 	<?php if(isset($_SESSION["payment_failed"])): ?>
 		<div class="alert alert-danger" role="alert">
@@ -40,7 +40,7 @@ function add(item_id, delta) {
 			<img src="/image.php?id=<?= $cart_item["product_id"] ?>" alt="" class="w-100"></a>
 		</div>
 		<div class="col-9 col-lg-10">
-			<h2><?= $cart_item['name'] ?></h2>
+			<h3><?= $cart_item['name'] ?></h3>
 			<p>
 				<label for="<?= $cart_item['product_id']?>">quantity</label>
 				<button type="button" class="btn btn-outline-primary btn-sm ms-1 font-monospace d-md-none" onclick=add(<?= $cart_item['product_id']?>,-1)>-</button>
