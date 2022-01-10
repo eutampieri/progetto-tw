b6ac21784f5f416c8cf01c66778dc5cc057014db
Eugenio Tampieri <eugenio.tampieri@studio.unibo.it>
2022-01-10 12:51:25 +0100

    Don't actually delete products

--- delete_product.php
+++ delete_product.php
@@ -7,7 +7,7 @@ if(!(isset($_SESSION) && $_SESSION["admin"]==1)) {
 	die("unauthorized request");
 }
 $db = get_db();
-$delete_query = $db->prepare("delete from product where id=:id");
+$delete_query = $db->prepare("UPDATE product SET deleted = 0, quantity=0 WHERE id=:id");
 $delete_query->bindParam(":id",$_POST["id"]);
 $delete_query->execute();
 
