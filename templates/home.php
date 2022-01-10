554e8956e217f6263307887efd6e49144b4f0108
Eugenio Tampieri <eugenio.tampieri@studio.unibo.it>
2022-01-10 10:30:39 +0100

    Fix image accessibility

--- templates/home.php
+++ templates/home.php
@@ -4,9 +4,9 @@ require_once(dirname(dirname(__FILE__))."/utils.php");
 <div class="d-flex flex-wrap justify-content-around">
   <?php foreach($products as $product): ?>
   <div class="card m-2 product" >
-    <a href="/product.php?id=<?= $product["id"] ?>">
+    <!--a href="/product.php?id=<?= $product["id"] ?>"-->
     <img src="/image.php?id=<?= $product["id"] ?>" class="card-img-top"
-      alt=""></a>
+      alt=""><!--/a-->
     <div class="card-body">
       <a href="/product.php?id=<?= $product["id"] ?>">
       <h2><?= $product["name"] ?></h2>
