ae228f36bf5640c56ec107d76621dfbdeaad05be
Eugenio Tampieri <eugenio.tampieri@studio.unibo.it>
2022-01-10 18:38:25 +0100

    Add last status

--- admin_orders.php
+++ admin_orders.php
@@ -1,17 +1,27 @@
 <?php
 require_once("utils.php");
 
+function fix_record($x) {
+	global $stmt_for_status;
+	$stmt_for_status->bindParam(":id", $x["id"]);
+	$stmt_for_status->execute();
+	$x["last_status"] = $stmt_for_status->fetch()["status"];
+	return $x;
+}
+
 $db = get_db();
 session_start();
+
+$stmt_for_status = $db->prepare("SELECT status FROM order_update WHERE order_id = :id ORDER BY timestamp desc LIMIT 1");
 
 if(!(isset($_SESSION["admin"]) && $_SESSION["admin"]==1)) {
 	http_response_code(401);
 	die("unauthorized request");
 }
 
-$stmt = $db->prepare("SELECT `order`.id, MIN(order_update.timestamp) AS `date`, `order`.payment_id FROM `order`, order_update WHERE `order`.id = order_update.order_id GROUP BY order_update.order_id ORDER BY `date` DESC");
+$stmt = $db->prepare("SELECT `order`.id, MIN(order_update.timestamp) AS `date` FROM `order`, order_update WHERE `order`.id = order_update.order_id GROUP BY order_update.order_id ORDER BY `date` DESC");
 $stmt->execute();
-$orders = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$orders = array_map(fix_record ,$stmt->fetchAll(PDO::FETCH_ASSOC));
 
 $page_title = "Lista ordini";
 $head_template = "page_head.php";
--- templates/admin_orders_t.php
+++ templates/admin_orders_t.php
@@ -4,6 +4,7 @@
 		<tr>
 			<th scope="col">Data</th>
 			<th scope="col">Numero d'ordine</th>
+			<th scope="col">Ultimo aggiornamento</th>
 			<th scope="col"></th>
 		</tr>
 	</thead>
@@ -12,6 +13,7 @@
 		<tr>
 			<td><?= date("d/m/Y H:i", intval($order["date"])) ?></td>
 			<td><?= $order["id"] ?></td>
+			<td><?= $order["last_status"] ?></td>
 			<td>
 				<a class="btn btn-info" role="button" href="/modify_order.php?order_id=<?= $order["id"] ?>">
 					<i class="fa fa-pencil" aria-hidden="true"></i>
